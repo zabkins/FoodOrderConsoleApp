dc6e29ab79c382e589712ce372526a7304b13d39
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-12 21:08:53 +0200

    Makes FoodOrder welcome guest and print available dishes

--- app/src/main/java/pl/zarczynski/foodorder/FoodOrder.java
+++ app/src/main/java/pl/zarczynski/foodorder/FoodOrder.java
@@ -2,11 +2,29 @@ package pl.zarczynski.foodorder;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import pl.zarczynski.foodorder.domain.Dish;
+import pl.zarczynski.foodorder.service.DishService;
+import pl.zarczynski.foodorder.service.OrderService;
+import pl.zarczynski.foodorder.view.View;
+
+import java.util.List;
 
 @Component
 public class FoodOrder implements CommandLineRunner {
+    private final DishService dishService;
+    private final OrderService orderService;
+    private final View view;
+
+    public FoodOrder(DishService dishService, OrderService orderService, View view) {
+        this.dishService = dishService;
+        this.orderService = orderService;
+        this.view = view;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+        view.welcomeClient();
+        List<Dish> allDishes = dishService.getAllDishes();
+        view.printDishes(allDishes);
     }
 }
