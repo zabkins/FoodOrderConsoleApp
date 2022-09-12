4cccb9c17e587fa667d71826592781413db20779
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-12 13:08:46 +0200

    Changes way of inserting inital data
    Deletes data.sql file
    Creates DataLoader class which will load initial data using JPA

---
+++ app/src/main/java/pl/zarczynski/foodorder/data/DataLoader.java
@@ -0,0 +1,78 @@
+package pl.zarczynski.foodorder.data;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pl.zarczynski.foodorder.domain.Dish;
+import pl.zarczynski.foodorder.domain.Ingredient;
+import pl.zarczynski.foodorder.domain.IngredientType;
+import pl.zarczynski.foodorder.repository.DishRepository;
+import pl.zarczynski.foodorder.repository.IngredientRepository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private Map<String, Ingredient> ingredientsMap;
+    private final IngredientRepository ingredientRepository;
+    private final DishRepository dishRepository;
+
+    public DataLoader(IngredientRepository ingredientRepository, DishRepository dishRepository) {
+        this.ingredientRepository = ingredientRepository;
+        this.dishRepository = dishRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        createIngredients();
+        createAvailableDishes();
+    }
+
+    private void createAvailableDishes() {
+        Dish beefBurger = new Dish();
+        beefBurger.setName("Beef Burger");
+        beefBurger.setPrice(35);
+        beefBurger.setIngredients(List.of(
+                ingredientsMap.get("BUN"),
+                ingredientsMap.get("BEEF"),
+                ingredientsMap.get("TOMATO"),
+                ingredientsMap.get("LETTUCE"),
+                ingredientsMap.get("ONION"),
+                ingredientsMap.get("CHEDDAR"),
+                ingredientsMap.get("BBQ SAUCE")
+        ));
+        dishRepository.save(beefBurger);
+    }
+
+    private void createIngredients() {
+        List<Ingredient> ingredients = List.of(
+                new Ingredient("PASTA",IngredientType.GRAINS),
+                new Ingredient("RICE",IngredientType.GRAINS),
+                new Ingredient("BREAD",IngredientType.GRAINS),
+                new Ingredient("BUN",IngredientType.GRAINS),
+                new Ingredient("BEEF",IngredientType.MEAT),
+                new Ingredient("CHICKEN",IngredientType.MEAT),
+                new Ingredient("POTATO",IngredientType.VEGETABLE),
+                new Ingredient("TOMATO",IngredientType.VEGETABLE),
+                new Ingredient("LETTUCE",IngredientType.VEGETABLE),
+                new Ingredient("CUCUMBER",IngredientType.VEGETABLE),
+                new Ingredient("ONION",IngredientType.VEGETABLE),
+                new Ingredient("BROCCOLI",IngredientType.VEGETABLE),
+                new Ingredient("ORANGE",IngredientType.FRUIT),
+                new Ingredient("MANGO",IngredientType.FRUIT),
+                new Ingredient("APPLE",IngredientType.FRUIT),
+                new Ingredient("EGG",IngredientType.DAIRY),
+                new Ingredient("CHEDDAR",IngredientType.DAIRY),
+                new Ingredient("MOZZARELLA",IngredientType.DAIRY),
+                new Ingredient("KETCHUP",IngredientType.OTHER),
+                new Ingredient("MAYONNAISE",IngredientType.OTHER),
+                new Ingredient("GARLIC SAUCE",IngredientType.OTHER),
+                new Ingredient("CHEESE SAUCE",IngredientType.OTHER),
+                new Ingredient("BBQ SAUCE",IngredientType.OTHER)
+        );
+        ingredientRepository.saveAll(ingredients);
+        ingredientsMap = ingredientRepository.findAll().stream()
+                .collect(Collectors.toMap(Ingredient::getName, ingredient -> ingredient));
+    }
+}
