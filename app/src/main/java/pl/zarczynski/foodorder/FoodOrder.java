39c666e22c32a3750056f5829c5b782885bd4b37
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-12 13:07:51 +0200

    Adds DefaulfDishService class

---
+++ app/src/main/java/pl/zarczynski/foodorder/FoodOrder.java
@@ -0,0 +1,12 @@
+package pl.zarczynski.foodorder.view;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FoodOrder implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ app/src/main/java/pl/zarczynski/foodorder/service/DefaultDishService.java
@@ -0,0 +1,2 @@
+package pl.zarczynski.foodorder.service;public class DefaultDishService {
+}
