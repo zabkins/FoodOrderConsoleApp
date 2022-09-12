9c234338f170d7b4fe6931a3696ef84ec350ae9f
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-12 21:08:23 +0200

    Makes DishRepository fetch dishes' ingredients

--- app/src/main/java/pl/zarczynski/foodorder/repository/DishRepository.java
+++ app/src/main/java/pl/zarczynski/foodorder/repository/DishRepository.java
@@ -1,8 +1,15 @@
 package pl.zarczynski.foodorder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.zarczynski.foodorder.domain.Dish;
+
+import java.util.List;
+
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Long> {
+    @Override
+    @Query("SELECT d FROM Dish d LEFT JOIN FETCH d.ingredients")
+    List<Dish> findAll();
 }
