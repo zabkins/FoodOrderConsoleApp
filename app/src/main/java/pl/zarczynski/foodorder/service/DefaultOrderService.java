8d560dbf17e2b9b36ca4cb0b2c8af77b8bbf4d88
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-13 13:12:18 +0200

    Makes OrderService update order's positions

--- app/src/main/java/pl/zarczynski/foodorder/service/DefaultOrderService.java
+++ app/src/main/java/pl/zarczynski/foodorder/service/DefaultOrderService.java
@@ -2,9 +2,11 @@ package pl.zarczynski.foodorder.service;
 
 import org.springframework.stereotype.Service;
 import pl.zarczynski.foodorder.domain.Order;
+import pl.zarczynski.foodorder.domain.OrderPosition;
 import pl.zarczynski.foodorder.repository.OrderRepository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,5 +22,31 @@ public class DefaultOrderService implements OrderService{
     @Override
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
+    }
+
+    @Override
+    public void updateOrdersPositions(Order order, OrderPosition orderPosition) {
+        List<OrderPosition> orderPositions = order.getOrderPositions();
+        if(orderPositions != null){
+            boolean isAlreadyAdded = false;
+            for (OrderPosition currentOrderPosition : orderPositions) {
+                if(currentOrderPosition.getDish().equals(orderPosition.getDish())){
+                    isAlreadyAdded = true;
+                    if(orderPosition.getAmount() == 0){
+                        order.removePosition(currentOrderPosition);
+                    } else {
+                        currentOrderPosition.setAmount(orderPosition.getAmount());
+                    }
+                }
+            }
+            if(!isAlreadyAdded){
+                order.addPosition(orderPosition);
+            }
+        } else {
+            orderPositions = new ArrayList<>();
+            orderPositions.add(orderPosition);
+            order.setOrderPositions(orderPositions);
+        }
+        order.updatePrice();
     }
 }
--- app/src/main/java/pl/zarczynski/foodorder/service/OrderService.java
+++ app/src/main/java/pl/zarczynski/foodorder/service/OrderService.java
@@ -1,9 +1,12 @@
 package pl.zarczynski.foodorder.service;
 
 import pl.zarczynski.foodorder.domain.Order;
+import pl.zarczynski.foodorder.domain.OrderPosition;
 
 import java.util.List;
 
 public interface OrderService {
     List<Order> getAllOrders();
+
+    void updateOrdersPositions(Order order, OrderPosition orderPosition);
 }
